5f4bbc80274525727655bfd2e6ea80b9e1f5e8ac
Jeongtae Kim <35194820+Origogi@users.noreply.github.com>
2022-10-23 09:41:17 +0900

    Time: 70 ms (95.02%), Space: 43.2 MB (92.29%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let result = nums[0];
+    let count = 1;
+    
+    for (let i = 1;i<nums.length;i++) {
+        if (result === nums[i]) {
+          count++;  
+        } else {
+            count--;
+            
+            if (count <0 ){
+                count = 1;
+                result = nums[i];
+            }
+        }
+    }
+    
+    return result;
+};
