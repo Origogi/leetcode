3d2153a2c6b2f8a4d2f46c9d1ec72da07a202e0c
Jeongtae Kim <35194820+Origogi@users.noreply.github.com>
2022-10-26 13:40:11 +0900

    Time: 126 ms (27.63%), Space: 43.5 MB (56.83%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isUgly = function(n) {
+    return check(n);
+};
+
+function check(n) {
+    const prime = [ 2, 3, 5];
+    
+    if (n === 1 || prime.includes(n)) {
+        return true;
+    } else if (n ===0 ) {
+        return false;
+    }
+    
+    
+    for (let i= 0;i<prime.length;i++) {
+        if (n % prime[i] === 0) {
+            return check(n / prime[i]);
+        }
+    }
+    
+    return false;
+    
+}
