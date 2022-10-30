14f2daaadb90f4eb1e7ebfc212d1e7d0a95584ee
Jeongtae Kim <35194820+Origogi@users.noreply.github.com>
2022-10-30 09:53:16 +0900

    Time: 187 ms (44.38%), Space: 45.8 MB (44.75%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -9,20 +9,10 @@ var firstUniqChar = function(s) {
         map[s[i]] = (map[s[i]] === undefined ? 0 : map[s[i]]) + 1;
     }
     
-    const entries = Object.entries(map);
-    
-    let targetKey;
-    
-    for (let i = 0; i < entries.length;i++) {
-        if (entries[i][1] === 1) {
-            targetKey = entries[i][0];
-            break;
-        }
-    }
-    
-    
+        
+   
     for (let i = 0;i<s.length;i++) {
-        if (targetKey === s[i]) {
+        if (map[s[i]] === 1) {
             return i;
         }
     }    
