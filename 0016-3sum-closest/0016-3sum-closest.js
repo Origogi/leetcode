f1e15b46f17301a9951f93647f95f92263b1034a
Jeongtae Kim <rlawjdxo1212@naver.com>
2022-11-15 10:40:51 +0900

    Time: 144 ms (80.27%), Space: 44.3 MB (46.14%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+  nums.sort((a, b) => (a - b));
+    
+    let result =  nums[0] + nums[1] + nums[2];
+    
+    for (let i = 0;i< nums.length -2;i++) {
+        let j = i +1;
+        let k = nums.length -1;
+        
+        while (j < k) {
+            let sum = nums[i] + nums[j] +nums[k];
+            
+            if (Math.abs(sum - target) < Math.abs(result - target)) {
+                result = sum;
+            }
+             
+            if (result == target) {
+                return result;
+            }
+            else if (sum > target) {
+                k--;
+            } else {
+                j++;
+            }
+            
+        }
+        
+        
+    }
+    return result;
+    
+};
