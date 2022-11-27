bf9236f7f80621bf856ed39dcb1c44f5a0783cdd
Jeongtae Kim <rlawjdxo1212@naver.com>
2022-11-27 16:15:01 +0900

    Time: 838 ms (9.16%), Space: 52.8 MB (5.05%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var repeatedSubstringPattern = function(s) {
+
+    
+    for (var i = 1;i<s.length;i++) {
+        if (s.length % i !== 0) {
+            continue;
+        }
+        
+        const splited = chunkString(s, i);
+        console.log(splited);
+        
+        var finded = true;
+        for (var j = 1; j<splited.length;j++) {
+            if (splited[0] !== splited[j]) {
+                finded = false;
+                break;
+            }
+        }
+        
+        if (finded) {
+            return true;
+        }
+        
+    }
+    
+    return false;
+};
+
+function chunkString (str, len) {
+  const size = Math.ceil(str.length/len)
+  const r = Array(size)
+  let offset = 0
+  
+  for (let i = 0; i < size; i++) {
+    r[i] = str.substr(offset, len)
+    offset += len
+  }
+  
+  return r
+}
