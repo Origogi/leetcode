10898195dfbfd1df9e77d2635a81de069b1cf6c3
Jeongtae Kim <rlawjdxo1212@naver.com>
2022-12-07 13:53:39 +0900

    Time: 171 ms (68.38%), Space: 57.6 MB (14.92%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
@@ -4,8 +4,7 @@
  */
 var findDisappearedNumbers = function(nums) {
     const subSet = new Set(nums);
-    const totalSet = new Set(Array.from({length : nums.length}, (_,i) => i + 1));
-    const difference = new Set([...totalSet].filter(x => !subSet.has(x)));
+    const totalList = Array.from({length : nums.length}, (_,i) => i + 1);
+    return [...totalList].filter(x => !subSet.has(x));
 
-    return [...difference];
 };
