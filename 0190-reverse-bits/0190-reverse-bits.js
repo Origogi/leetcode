37d2d1879e3fd352f13e2289ac74c3497e35de5b
Jeongtae Kim & tony.origogi <rlawjdxo1212@naver.com>
2023-07-24 08:33:50 +0900

    Time: 65 ms (64.32%), Space: 44.3 MB (36.72%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.js
+++ 0190-reverse-bits/0190-reverse-bits.js
@@ -3,16 +3,15 @@
  * @return {number} - a positive integer
  */
 var reverseBits = function(n) {
-    const binary = convertToFixedLengthBinary(n ,32);
-    const array = binary.split("");
-    const reverseBinary = array.reduce((a,b) => `${b}${a}`);
-    console.log(reverseBinary);
+    let ret = 0;
     
-    return parseInt(reverseBinary,2);
+  for (let i = 0; i < 32; i++) {
+        ret = (ret << 1) | (n & 1);
+        n >>= 1;
+    }
+    
+    return ret >>> 0;
+    
+    
 };
 
-function convertToFixedLengthBinary(n, length) {
-  const binaryString = (n >>> 0).toString(2);
-  const padding = "0".repeat(Math.max(0, length - binaryString.length));
-  return padding + binaryString;
-}
