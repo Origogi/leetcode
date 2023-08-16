a65d25474dfd157795f68006b1775654dab32065
Jeongtae Kim & tony.origogi <rlawjdxo1212@naver.com>
2023-08-16 09:43:03 +0900

    Time: 78 ms (67.14%), Space: 49 MB (5.91%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} score
+ * @return {string[]}
+ */
+var findRelativeRanks = function(score) {
+    const sortedScore = [...score].sort((a,b) => b- a);
+    var reducer = function(acc, value, index, array) {
+      acc[value] = (index + 1).toString();
+      return acc;
+    }
+    const map = sortedScore.reduce(reducer, {});
+    console.log(map);
+
+    return score.map((e)  => {
+        const rank = map[e];
+        if (rank === '1') {
+            return 'Gold Medal';
+        } else if ( rank=== '2') {
+            return 'Silver Medal';
+        } else if (rank === '3') {
+            return 'Bronze Medal';
+        }
+        return rank;
+    });
+    
+    
+};
