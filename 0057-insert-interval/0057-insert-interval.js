792cf3ab339e00ffbd7aaab62ed70339ebc8c646
Jeongtae Kim & tony.origogi <rlawjdxo1212@naver.com>
2023-10-27 10:24:11 +0900

    Time: 67 ms (41.37%), Space: 47.3 MB (5.80%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.js
+++ 0057-insert-interval/0057-insert-interval.js
@@ -4,20 +4,21 @@
  * @return {number[][]}
  */
 var insert = function(intervals, newInterval) {
-    const merged = [...[...intervals, newInterval].sort((a,b) => a[0] - b[0])];
 
-    return merged.reduce((acc, item) => {
-        if (acc.length ===0) {
-            return [item];
-        }
-        const top = acc.pop();
-        
-        if (top[1] < item[0]) {  // no overlap
-            return [...acc, top, item];
-        } else {
-            const overlap = [top[0], Math.max(top[1], item[1])];
-            return [...acc, overlap];
-        }
-    }, []);
+    return [...[...intervals, newInterval]
+        .sort((a,b) => a[0] - b[0])]
+        .reduce((acc, item) => {
+            if (acc.length ===0) {
+                return [item];
+            }
+            const top = acc.pop();
+
+            if (top[1] < item[0]) {  // no overlap
+                return [...acc, top, item];
+            } else {
+                const overlap = [top[0], Math.max(top[1], item[1])];
+                return [...acc, overlap];
+            }
+        }, []);
     
 };
