dae17c37324bdadea4a8abb4e9ab12af32d78731
Jeongtae Kim & tony.origogi <rlawjdxo1212@naver.com>
2023-10-29 09:40:12 +0900

    Time: 46 ms (87.00%), Space: 42.4 MB (16.83%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number} n
+ * @return {number[][]}
+ */
+var generateMatrix = function(n) {
+    
+    const ret = Array.from(Array(n), () => new Array(n));
+    
+    
+    const dirX = [0,1,0,-1];
+    const dirY = [1,0,-1,0];
+    let dirIdx = 0;
+    
+    let position = { x: 0, y:0};
+    
+    for (let i = 1;i<= n *n;i++) {
+        const {x, y} = position;
+        
+        ret[x][y] = i;
+        
+        let nextPosition = {
+            x : x + dirX[dirIdx],
+            y : y + dirY[dirIdx]
+        }
+        
+        if(!validate(nextPosition, n, ret)) {
+            dirIdx++;
+            dirIdx %= 4;
+            
+            nextPosition = {
+                x : x + dirX[dirIdx],
+                y : y + dirY[dirIdx]
+            }
+        }
+        position = nextPosition;
+    }
+    
+    
+    return ret;
+    
+};
+
+
+function validate(position, n, map) {
+    const {x, y} = position;
+    return x >= 0 && x < n && y>=0 && y <n && map[x][y] === undefined;
+}
