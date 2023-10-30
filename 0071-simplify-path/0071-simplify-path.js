fb4ba47dca01b0fd9f1ecfc3fbec410dbde8805b
Jeongtae Kim & tony.origogi <rlawjdxo1212@naver.com>
2023-10-30 10:35:36 +0900

    Time: 75 ms (13.08%), Space: 45.7 MB (13.14%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.js
+++ 0071-simplify-path/0071-simplify-path.js
@@ -3,17 +3,18 @@
  * @return {string}
  */
 var simplifyPath = function(path) {
-    const filterCondition = (s) => s.length > 0 || s === '.';
+    const filterCondition = (s) => s.length > 0 && s !== '.';
+    
     const tokens = path.split("/").filter(filterCondition);
+    console.log(tokens);
     
     return '/' + tokens.reduce((acc, token) => {
          if (token === '..') {
-             if (acc.length > 0) {
-                             acc.pop();
-             }
-        } else if (token !== '.') {
+            acc.pop();
+             
+        } else {
             acc.push(token);
-        }    
+        }
         return acc;
     }, [
        
