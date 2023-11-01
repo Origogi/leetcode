0aeae14eae22b9dbfd1fad8c87c480b6fa814db9
Jeongtae Kim & tony.origogi <rlawjdxo1212@naver.com>
2023-11-01 11:05:57 +0900

    Time: 64 ms (6.69%), Space: 43.6 MB (6.91%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    const countMap = nums.reduce((acc,a) => {
+        acc[a] = acc[a] + 1;
+        return acc;
+    },{
+        '0': 0,
+        '1' :0,
+        '2' : 0
+    });
+    
+    console.log(countMap);
+    
+    let i = 0;
+    
+    const keys = [0, 1, 2];
+    
+    while (i < nums.length) {
+        for (const k of keys) {
+            const countOfKey = countMap[k];
+            if (countOfKey) {
+                nums[i] = k;
+                countMap[k] = countOfKey -1;
+                break;
+            }
+        }
+        i++;
+    }
+    
+    
+    
+    
+};
