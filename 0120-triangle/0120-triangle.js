7769ff4c34d4c9a7a86966fa91663dadadc49477
Jeongtae Kim & tony.origogi <rlawjdxo1212@naver.com>
2023-11-16 11:20:21 +0900

    Time: 68 ms (11.24%), Space: 44.1 MB (25.74%) - LeetHub

--- 0120-triangle/0120-triangle.js
+++ 0120-triangle/0120-triangle.js
@@ -4,13 +4,14 @@
  */
 var minimumTotal = function(triangle) {
     const getArrSafe = (i, arr) => arr[i] ?? Number.MAX_SAFE_INTEGER;
-    const getMin = (i, arr) => Math.min(getArrSafe(i -1 , arr), getArrSafe(i, arr));
+    const min = (a,b) => Math.min(a,b)
+    const getMinLeftRight = (i, arr) => min(getArrSafe(i -1 , arr), getArrSafe(i, arr));
     
     return triangle.reduce((acc, row) => 
          Array
             .from({length : row.length}, () => Number.MAX_SAFE_INTEGER)
-            .map((item, i) => row[i] + getMin(i, acc))
+            .map((item, i) => row[i] + getMinLeftRight(i, acc))
         
-    ).reduce((a,b) => Math.min(a, b));
+    ).reduce(min);
     
 };
