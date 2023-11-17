06c39ae58685bfe31904d13bf924a2c8bffcc522
Jeongtae Kim & tony.origogi <rlawjdxo1212@naver.com>
2023-11-17 16:30:01 +0900

    Time: 322 ms (42.66%), Space: 202.9 MB (5.05%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+    if (n <= 1) {
+        return 0;
+    }
+    
+    const isPrimeNum = Array(n).fill(true);
+    
+    for (let i = 2;i<= Math.sqrt(n);i++) {
+        if (isPrimeNum[i]) {
+            let j = i + i;
+            while(j<isPrimeNum.length) {
+                isPrimeNum[j] = false;
+                j +=i;
+            }
+        }
+    }
+    console.log(isPrimeNum);
+    
+    return isPrimeNum.slice(2).filter(a => a).length;
+    
+    
+};
