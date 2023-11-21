c1995b052aa8e415dbf94986ad8503140b1509d4
Jeongtae Kim & tony.origogi <rlawjdxo1212@naver.com>
2023-11-21 10:52:22 +0900

    Time: 67 ms (16.15%), Space: 44.6 MB (20.81%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var restoreIpAddresses = function(s) {
+
+    const ret = [];
+    
+    const splitLength  = (str, length) => [str.substring(0, length), str.slice(length)];
+    const isValidIp = (str) => parseInt(str) <= 255;
+    
+    function getIp(prefix, remain) {
+        if (prefix.length === 4 && remain === '') {
+            ret.push(prefix.join('.'));
+            return;
+        } else if (prefix.length > 4) {
+            return;
+        }
+        
+        if (remain.charAt(0) === '0') {
+            const splited = splitLength(remain, 1);
+            getIp([...prefix, splited[0]], splited[1]);
+            return;
+        }
+        
+        const end = Math.min(3, remain.length);
+        
+        for (let i = 1;i<=end;i++) {
+            const splited = splitLength(remain, i);
+            
+            if (isValidIp(splited[0])) {
+                getIp([...prefix, splited[0]], splited[1]);
+            }
+            
+        }
+    }
+    
+    getIp([],s);
+    
+    return ret;
+};
