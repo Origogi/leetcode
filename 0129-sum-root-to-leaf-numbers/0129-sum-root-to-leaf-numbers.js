33dfe36c3ae5992a9fb9afd3fb4550513428827c
Jeongtae Kim & tony.origogi <rlawjdxo1212@naver.com>
2023-12-07 16:31:55 +0900

    Time: 56 ms (36.21%), Space: 42.3 MB (61.55%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.js
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.js
@@ -11,27 +11,20 @@
  * @return {number}
  */
 var sumNumbers = function(root) {
-    
-    let sum = 0;
-    
-    function dfs(root, pathValue) {
-        if (!root) {
-            return;
-        }
-        
-        const curValue = pathValue * 10 + root.val;
-
-        
-        if (!root.left && !root.right) {  //leaf
-            sum += curValue;
-            return;
-        }
-        
-        dfs(root.left, curValue);
-        dfs(root.right, curValue);
-    }
-    
-    dfs(root, 0);
-    
-    return sum;
+    return dfs(root, 0);
 };
+
+function dfs(root, pathValue) {
+    if (!root) {
+        return 0;
+    }
+
+    const curValue = pathValue * 10 + root.val;
+
+    if (!root.left && !root.right) {  //leaf
+        return curValue;
+    }
+
+    return dfs(root.left, curValue) + dfs(root.right, curValue);
+}
+    
