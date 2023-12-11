9e87ccf9cd2fb39938c39f2043aa2700015e61b7
Jeongtae Kim & tony.origogi <rlawjdxo1212@naver.com>
2023-12-11 22:08:12 +0900

    Time: 119 ms (5.83%), Space: 50.8 MB (5.83%) - LeetHub

---
+++ 0256-paint-house/0256-paint-house.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} costs
+ * @return {number}
+ */
+var minCost = function(costs) {
+    
+    return costs.reduce((acc, cost) => {
+        const nextCost = [0, 0, 0];
+        
+        nextCost[0] = cost[0] + Math.min(acc[1], acc[2]);
+        nextCost[1] = cost[1] + Math.min(acc[0], acc[2]);
+        nextCost[2] = cost[2] + Math.min(acc[0], acc[1]);
+        
+        console.log(nextCost);
+        return nextCost;
+    }).reduce((a,b) => Math.min(a,b));
+    
+        
+};
