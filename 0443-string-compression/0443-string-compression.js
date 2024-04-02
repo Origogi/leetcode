4dc1baf369dffffb2c087728dda6cf0146c5c91f
Jeongtae Kim & tony.origogi <rlawjdxo1212@naver.com>
2024-04-02 21:46:11 +0900

    Time: 57 ms (82.96%), Space: 52.7 MB (10.92%) - LeetHub

--- 0443-string-compression/0443-string-compression.js
+++ 0443-string-compression/0443-string-compression.js
@@ -9,16 +9,18 @@ var compress = function(chars) {
     let count = 0
     let c = chars[0]
     
+    const appendCompress = (origin, c, count) => `${origin}${c}${count > 1 ? count : ''}`
+    
     for (let i = 0;i< chars.length;i++) {
         if (c !== chars[i]) {
-            compress = `${compress}${c}${count > 1 ? count : ''}`
+            compress = appendCompress(compress, c, count)
             count = 0
         }
         c = chars[i]
         count++
     }
     
-    compress = `${compress}${c}${count > 1 ? count : ''}`
+    compress = appendCompress(compress, c, count)
     const compressTokens = compress.split('')
     
     
